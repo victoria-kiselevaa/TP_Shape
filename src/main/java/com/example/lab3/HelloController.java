674885a72f0ea9b579c514caa27e3c73be439a75
viktoria-kiselevaa <victoryk1seleva@yandex.ru>
2024-11-10 18:35:46 +0300

    Добавлен хранитель

--- src/main/java/com/example/lab3/HelloController.java
+++ src/main/java/com/example/lab3/HelloController.java
@@ -1,32 +1,96 @@
 package com.example.lab3;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import model.Hexagon;
-import model.Line;
-import model.Rectangle;
+import model.*;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     public Canvas canvas;
     public ColorPicker color;
     public TextField textF;
+    ShapeFactory shapeFactory = new ShapeFactory();
+    Shape shape=null;
+    GraphicsContext gr;
+    private boolean isDragging = false;
+    private double dragOffsetX = 0;
+    private double dragOffsetY = 0;
+    private MemoSelect memoSelect = new MemoSelect();
+    private Memento temp = null;
 
     public void onMouseClick(MouseEvent mouseEvent) {
-        GraphicsContext gr = canvas.getGraphicsContext2D();
+        double x = mouseEvent.getX();
+        double y = mouseEvent.getY();
+
         gr.clearRect(0,0,canvas.getWidth(),canvas.getHeight());
-        String name=textF.getText();
-        if(name.equals("Прямоугольник")){
-            Rectangle rectangle = new Rectangle(mouseEvent.getX(), mouseEvent.getY(), color.getValue(),100, 50);
-            rectangle.draw(gr);
-        } else if (name.equals("Линия")) {
-            Line line=new Line(mouseEvent.getX(), mouseEvent.getY(),color.getValue(),200,45);
-            line.draw(gr);
-        } else if (name.equals("Шестиугольник")) {
-            Hexagon hexagon=new Hexagon(mouseEvent.getX(), mouseEvent.getY(),color.getValue());
-            hexagon.draw(gr);
+
+        shape=shapeFactory.createShape(Integer.parseInt(textF.getText()), x,y);
+        shape.setColor(color.getValue());
+        shape.draw(gr);
+
+        dragOffsetX = mouseEvent.getX() - shape.getX();
+        dragOffsetY = mouseEvent.getY() - shape.getY();
+
+        newMemento();
+
+    }
+
+    public void OnClick(ActionEvent actionEvent) {
+        if(shape!=null){
+            shape.setColor(color.getValue());
+            shape.draw(gr);
+            newMemento();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        gr = canvas.getGraphicsContext2D();
+    }
+
+
+    public void MouseDragged(MouseEvent mouseEvent) {
+        if (shape != null) {
+            isDragging = true;
+            double newX = mouseEvent.getX() - dragOffsetX;
+            double newY = mouseEvent.getY() - dragOffsetY;
+            shape.setPosition(newX, newY);
+            gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            shape.draw(gr);
+        }
+    }
+
+    public void MouseReleased(MouseEvent mouseEvent) {
+        if (isDragging) {
+            isDragging = false;
+            double newX = mouseEvent.getX() - dragOffsetX;
+            double newY = mouseEvent.getY() - dragOffsetY;
+            shape.setPosition(newX, newY);
+            gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            shape.draw(gr);
+            newMemento();
+        }
+    }
+
+    public void ButReturnClick(ActionEvent actionEvent) {
+        if (!memoSelect.mementoList.isEmpty()) {
+            shape = memoSelect.poll().getState();
+            gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            shape.draw(gr);
+        }else {
+            newMemento();
+        }
+    }
+
+    public void newMemento(){
+        temp = new Memento(shape);
+        memoSelect.push(temp);
     }
 }
--- src/main/java/model/Shape.java
+++ src/main/java/model/Shape.java
@@ -3,7 +3,7 @@ package model;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-abstract class Shape {
+public abstract class Shape {
     protected double x;
     protected double y;
     protected Color color;
